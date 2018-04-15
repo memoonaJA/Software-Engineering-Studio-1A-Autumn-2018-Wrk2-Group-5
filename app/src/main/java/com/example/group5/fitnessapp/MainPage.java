3852e10e214b13a68cc626c555449f7eac2db265
nath1100 <nath.luu1@gmail.com>
2018-04-15 17:34:01 +1000

    Fixed UI errors and added basic navigation

--- app/src/main/java/com/example/group5/fitnessapp/MainPage.java
+++ app/src/main/java/com/example/group5/fitnessapp/MainPage.java
@@ -1,7 +1,9 @@
 package com.example.group5.fitnessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPage extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    // called when user presses login
+    public void attemptLogin(View view) {
+        Intent intent = new Intent(this, Self.class);
+        // Code to authenticate user login should be triggered here.
+        if (true/**Authentication code should return true/false here. temp set to true**/) {
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/group5/fitnessapp/Self.java
+++ app/src/main/java/com/example/group5/fitnessapp/Self.java
@@ -1,7 +1,9 @@
 package com.example.group5.fitnessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Self extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Self extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self);
+    }
+
+    public void openStepsTracker(View view) {
+        Intent intent = new Intent(this, StepTracking.class);
+        startActivity(intent);
     }
 }
