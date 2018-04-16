4be9d58c03b83a14f4bc7077f5151629612fdc9d
ReMashed <ReMashed@users.noreply.github.com>
2018-04-16 21:56:10 +1000

    Step Counter with Accelerometer CLEANED UP

--- app/src/main/java/com/remashed/stepcounter/MainActivity.java
+++ app/src/main/java/com/remashed/stepcounter/MainActivity.java
@@ -13,13 +13,10 @@ import static java.lang.Math.*;
 //extends Activity class , inherits its methods,properties etcccccccccccccc
 public class MainActivity extends AppCompatActivity implements SensorEventListener{ //recieves notifcations from SensorManager for new sensor data
     private SensorManager mSensorManager; //Sensor Manager allows you to access sensors
-    private Sensor mStepCounterSensor; //API 19 Sensor object, Going to use properties in sensor class to get info related to a step counter sensor
-    private Sensor mAccelerometerSensor; //Same objective as above but for accelerometer
+    private Sensor mAccelerometerSensor; //API 19 sensor object, Gonna use it for accelerometer
     private TextView textview;
     private TextView sensorText;
-    private int stepCounter = 0;
-    private int counterSteps = 0;
-    private int steps = 0;
+    private int steps = 0; //Keeps track of steps taken
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,83 +25,58 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         textview = (TextView) findViewById(R.id.steptext); //assigns to a reference || Links to the textview with id provided in main xml
         sensorText = (TextView) findViewById(R.id.sensortext);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE); //Use getsystemservice to retrieve a sensorManager for accessing sensors
-       // mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER); //Gets the sensor for a step counter
         mAccelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        //NOTE: Step counter doesn't reset until you turn off the device
-
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) { //called when there is a new reading from the sensor using the same sensor values (but a newer timestamp)
-        //Note: of accelerometers value[0] = acceleration minus Gx on the x-axis
-        //http://blog.bawa.com/2013/11/create-your-own-simple-pedometer.html got the implementation here for what to do with the values.
-        float valueX = event.values[0]; //Data from sensor
-        float valueY = event.values[1];
-        float valueZ = event.values[2];
+        //Accelerometer provides acceleration in X, Y ,Z axis which includes force of gravity
 
-        double x = (double) event.values[0];
+        //Accelerometer returns float values
+        double x = (double) event.values[0]; //Convert acceleration values to doubles
         double y = (double) event.values[1];
         double z = (double) event.values[2];
-        //int steps = (int) valueX; //convert sensors data to an int
-        double sum = Math.pow(x,2) + Math.pow(y, 2) + Math.pow(z, 2);
-        double root = sqrt(sum);
-        /*if (valueX > 0 ) {//acceleration in device //dunno what this does
-            steps = (int) valueX;
-        } */
 
-        /* if (counterSteps < 1) { //This code here resets the step count when open the app again after closing it not minimise.
-            counterSteps = (int) event.values[0];
-        }
-        stepCounter = (int)event.values[0] - counterSteps; */
+        //Some accurate formula found of the net, to provide a data regardless of Device orientation
+        //Length of a vector is sqrt(XX + YY + ZZ) -> always ~9.8(force of gravity)  reading regardless of orientation
+        //I don't understand any of the mathematical reasoning but it works sorta
 
-        /*if (mStepCounterSensor != null){ //Checks if sensor exist
-            if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-                textview.setText("Step Count: " + stepCounter);
-            }
-        } else {
-            sensorText.setText("You device does not support the necessary sensors");
-            //use the accelerometer stuff here
-        } */
-        assert mSensorManager != null;
+        double sum = Math.pow(x,2) + Math.pow(y, 2) + Math.pow(z, 2); //Executing that formula
 
+        //Changing RAW DATA into usable data
+        double sensorData = sqrt(sum);
 
-         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-             int count = 0;
-
-             if(root >= 12 && root <= 17) {
-                 count++;
-
-
-             }steps = steps + count;
-             textview.setText("Steps: "+ steps);
-             sensorText.setText("length: " + root );
+         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)  //Checks if accelerometer is providing data
+         {
+             int count = 0; //count for when person walks/jogs/runs
+             if(sensorData>= 12 && sensorData <= 17) {
+                 count++; //When person walks increment the count
+             }
+             steps = steps + count; //Add the count to total steps taken
+             textview.setText("Steps: "+ steps); //print out total steps
+             sensorText.setText("Sensor Data (Vector Length): " + sensorData); //Displays meaningful sensor data
          }
-
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        //no need
+        //Empty code but must implement as its abstract
     }
 
     protected void onResume(){ //Register sensors as soon as activity is opened
         super.onResume();
-        mSensorManager.registerListener(this, mStepCounterSensor, SensorManager.SENSOR_DELAY_FASTEST);
         mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-   /* protected void onPause(){ //Occurs when activity is minimised, or some else appears in foreground but you can still see it
+    /* protected void onPause(){ //Occurs when activity is minimised, or some else appears in foreground but you can still see it
                                 //Do you want it to keep running? (Yes), but for now i'm leaving it off for testing and learn purposes
         super.onPause();
 
-        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mAccelerometerSensor);
     }
     */
     protected void onStop() { //When Activity is no longer visible, stop counting
         super.onStop();
-        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mAccelerometerSensor);
     }
-
-
-
 }
