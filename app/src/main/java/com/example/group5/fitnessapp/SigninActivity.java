858d46e962a7ed04a9f612c731600303fa0583de
nath1100 <nath.luu1@gmail.com>
2018-04-23 19:10:01 +1000

    implementing firebase db authentication

--- app/src/main/java/com/example/group5/fitnessapp/SigninActivity.java
+++ app/src/main/java/com/example/group5/fitnessapp/SigninActivity.java
@@ -1,7 +1,9 @@
 package com.example.group5.fitnessapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SigninActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class SigninActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
     }
+
+    private void triggerSignUp(View view) {
+        // TODO:Firebase database management
+        Intent intent = new Intent(this, LoginActivity.class);
+        this.finish();
+    }
+
 }
