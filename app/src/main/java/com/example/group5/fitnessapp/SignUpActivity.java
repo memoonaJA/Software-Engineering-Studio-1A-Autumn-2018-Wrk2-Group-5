3c6d210d901e630a77fb2d0381b9e9cae4f374d0
ReMashed <ReMashed@users.noreply.github.com>
2018-05-17 22:03:48 +1000

    Implemented a basic graph structure Todo: 1. Link graph to database data  1.1. Step count data in database, used for graph  1.2. Graph needs to change horizontal labels depending on the date  1.3. Need to be able to look at previous data (for user)

--- app/src/main/java/com/example/group5/fitnessapp/SignUpActivity.java
+++ app/src/main/java/com/example/group5/fitnessapp/SignUpActivity.java
@@ -3,11 +3,11 @@ package com.example.group5.fitnessapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SigninActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_signup);
     }
 }
